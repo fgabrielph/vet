a7c721ddf6551a702647d919261c48e762c5c21d
Franz <otameganegod@gmail.com>
2020-07-03 15:16:17 +0800

    Installed Auth

---
+++ resources/views/admin/master.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+        <title>{{config('app.name')}} | @yield('title')</title>
+
+        <!-- Font Awesome Icons -->
+        <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+        <!-- Theme style -->
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+    </head>
+
+    <body class="hold-transition sidebar-mini">
+        <div class="wrapper">
+
+            @include('admin.includes.navbar')
+            @include('admin.includes.sidebar')
+
+            <div class="content-wrapper">
+                @yield('content')
+            </div>
+
+            @include('admin.includes.footer')
+
+        </div>
+
+        <!-- REQUIRED SCRIPTS -->
+        <script src="{{asset('js/app.js')}}"></script>
+        @yield('scripts')
+
+    </body>
+</html>
